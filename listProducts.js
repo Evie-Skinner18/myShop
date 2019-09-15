1729b03b1fa205d5f90fd449623e3deb7716d60a
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-09-15 18:29:36 +0100

    Challenge complete, aww yeah

--- listProducts.js
+++ listProducts.js
@@ -2,7 +2,10 @@
 
 // using statement for faker library
 var faker = require('faker');
-var randomName = faker.name.findName();
-var randomProduct = faker.commerce.product();
 
-console.log(randomProduct);
+// if you want a different random output each time, don't store the faker API calls in vars
+console.log(`Hello there ${faker.name.findName()}. Welcome to Evie\'s Shop!`);
+
+for(let i = 0; i < 10; i++){
+    console.log(`This ${faker.commerce.productAdjective()} ${faker.commerce.product()} is a bargain at only £${faker.commerce.price()}!`);
+}
